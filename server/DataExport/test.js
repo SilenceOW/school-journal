e4d4947b5c3366e1edd7199e2b3c9c9485ce60ab
SilenceOW <mitrich25092006@gmail.com>
2022-10-02 16:20:10 +0400

    Models definition and signup method

---
+++ server/DataExport/test.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import Role from "./models/Role.js";
+import dotenv from 'dotenv';
+import Status from "./models/Status.js";
+dotenv.config({path: "./.env"});
+
+try {
+    await mongoose.connect(process.env.ATLAS_URI);
+    await Role.insertMany(
+        [
+            {
+                name: 'Учитель'
+            },
+            {
+                name: 'Директор',
+            },
+            {
+                name: 'Зам. Директора'
+            },
+            {
+                name: 'Ученик'
+            },
+            {
+                name: 'Администратор'
+            }
+        ],
+        (err, doc) => {
+            if (err) return console.log(err);
+            console.log("Сохранены объекты role");
+        })
+    await Status.insertMany(
+        [
+            {
+                name: 'Admin'
+            },
+            {
+                name: 'User'
+            }
+        ],
+        (err, doc) => {
+            if (err) return console.log(err);
+            console.log("Сохранены объекты Status")
+        }
+    )
+    mongoose.disconnect();
+} catch (error) {
+    console.log(error)
+}
